7f5c5cec8d956f030c086fedce41aa25a1e0b554
rhusai2 <89809324+rhusai2@users.noreply.github.com>
2021-11-11 23:17:34 -0600

    Update BaccaratGame.java

--- server/src/main/java/BaccaratGame.java
+++ server/src/main/java/BaccaratGame.java
@@ -5,13 +5,25 @@ public class BaccaratGame {
     ArrayList<Card> playerHand;
     ArrayList<Card> bankerHand;
     BaccaratDealer theDealer;
+    BaccaratGameLogic logic;
     double currentBet;
     double totalWinnings;
+    String betOn;
 
     //This method will determine if the user won or lost their bet and return the amount won or
     //lost based on the value in currentBet.
-    /*public double evaluateWinnings(){
-        return;
-    }*/
-
+    public double evaluateWinnings(){
+    	String winner = logic.whoWon(playerHand, bankerHand);
+    	
+    	if((betOn == winner && winner == "Player") || (betOn == winner && winner == "Banker")) {
+    		totalWinnings += currentBet;
+    	}
+    	else if(betOn == winner && winner == "Draw") {
+    		totalWinnings += currentBet*8;
+    	}
+    	else {
+    		totalWinnings -= currentBet;
+    	}
+    	return totalWinnings;
+    }
 }
