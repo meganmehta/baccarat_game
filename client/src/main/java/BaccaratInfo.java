42dadf9614619e9a419d5ff637f50647e7bc8aae
Megan <megandeemehta@gmail.com>
2021-11-16 18:42:13 -0600

    random changes

--- client/src/main/java/BaccaratInfo.java
+++ client/src/main/java/BaccaratInfo.java
@@ -1,8 +1,16 @@
-
-
-
+import java.util.ArrayList;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+import java.util.function.Consumer;
 
 public class BaccaratInfo implements Serializable{} {
     //This class will be used to send information back and forth between the server and clients.
+
+
+
+
 
 }
