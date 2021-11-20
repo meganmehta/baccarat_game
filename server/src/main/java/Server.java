a8431d80c9d7ccca2ea46a89e40f3e622fee6837
rhusai2 <89809324+rhusai2@users.noreply.github.com>
2021-11-20 13:21:04 -0600

    Update Server.java

--- server/src/main/java/Server.java
+++ server/src/main/java/Server.java
@@ -1,129 +1,27 @@
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
-import java.util.function.Consumer;
 
-import javafx.application.Platform;
-import javafx.scene.control.ListView;
+class BaccaratInfo implements Serializable{
+    double betAmount;
+    String userBetChoice;
+    BaccaratGame game = new BaccaratGame();
 
-public class Server{
+    //Receives the game object from client and other data members needed
+    game = getIntent().getSerializableExtra("GamePlay");
+    betAmount = getIntent().getSerializableExtra("Bet");
+    userBetChoice = getIntent().getSerializableExtra("Choice");
 
-    int count = 1;
-    ArrayList<ClientThread> clients = new ArrayList<ClientThread>();
-    TheServer server;
+    //list has to inclue:
+    //- how many clients are connected to the server.
+    //- The results of each game played by any client.
+    //- how much the a client bet on each game
+    //- how much a client won or lost on each game
+    //- if a client drops off the server.
+    //- if a new client joins the server.
+    //- is the client playing another hand.
 
-    private Consumer<Serializable> callback;
+    //BaccaratInfo class should only contain information like the bidding amount,
+    // who the user bet on, and the
+    // results of the game that has been played in the server
+    // (who won, what cards were dealt, total winnings, etc.).
 
-
-    Server(Consumer<Serializable> call){
-
-        callback = call;
-        server = new TheServer();
-        server.start();
-    }
-
-
-    public class TheServer extends Thread{
-
-        public void run() {
-
-            try(ServerSocket mysocket = new ServerSocket(5555);){
-                System.out.println("Server is waiting for a client!");
-
-
-                while(true) {
-
-                    ClientThread c = new ClientThread(mysocket.accept(), count);
-                    callback.accept("New client has connected to server: " + "Client #" + count);
-                    clients.add(c);
-                    callback.accept("Number of clients connected to server: " + clients.size()); //how many clients are connected to the server.
-                    c.start();
-
-                    count++;
-
-                }
-            }//end of try
-            catch(Exception e) {
-                callback.accept("Server socket did not launch");
-            }
-        }//end of while
-    }
-
-
-    class ClientThread extends Thread{
-
-
-        Socket connection;
-        int count;
-        ObjectInputStream in;
-        ObjectOutputStream out;
-        BaccaratGame game;
-
-
-        ClientThread(Socket s, int count){
-            this.connection = s;
-            this.count = count;
-        }
-
-        //updateClients goes on all client threads?
-        public void updateClients(String message) {
-            for(int i = 0; i < clients.size(); i++) {
-                ClientThread t = clients.get(i);
-                try {
-                    t.out.writeObject(message);
-                }
-                catch(Exception e) {}
-            }
-        }
-
-        public void run(){
-
-            try {
-                in = new ObjectInputStream(connection.getInputStream());
-                out = new ObjectOutputStream(connection.getOutputStream());
-                connection.setTcpNoDelay(true);
-                game = new BaccaratGame();
-
-            }
-            catch(Exception e) {
-                System.out.println("Streams not open");
-            }
-
-            updateClients("New client on server: Client #"+ count); //if a new client joins the server.
-
-            while(true) {
-                try {
-                    //include game related updates here?
-                    //list has to inclue:
-                    //- The results of each game played by any client.
-                    //- how much the a client bet on each game
-                    //- how much a client won or lost on each game
-                    //- if a client drops off the server.
-                    //- is the client playing another hand.
-                    game = new BaccaratGame();
-                    BaccaratInfo gameInfo = (BaccaratInfo) in.readObject();
-                    callback.accept("Client #" + count + " bet $" +
-                            gameInfo.betAmount + " on " + gameInfo.userBetChoice);
-
-                }
-                catch(Exception e) {
-                    callback.accept("Client #"+ count +" has left the server!");
-                    clients.remove(this);
-                    callback.accept("Number of clients connected to server: " + clients.size()); //how many clients are connected to the server.
-                    break;
-                }
-            }
-        }//end of run
-
-
-    }//end of client thread
 }
-
-
-
-
-
-
